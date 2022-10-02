0d32c59bf9d04bdd74f023b3c847607e89bb4edb
Riyad0821 <mdriyad0821@gmail.com>
2022-10-03 01:36:56 +0600

    Add home page design

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,17 @@
+import create_my_event from "../resource/create_my_event.png";
 const Button = ({ id = "btn", btnText = "Sample Button", btnSize = "sm" }) => {
   return (
-    <div>
-      <button>{btnText}</button>
+    <div className="items-center mx-auto">
+      <button
+        type="button"
+        class="text-white bg-gradient-to-r from-violet-500 to-fuchsia-400 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 "
+      >
+        <img
+          className="object-contain mx-3 my-3"
+          src={create_my_event}
+          alt="Event Card"
+        ></img>
+      </button>
     </div>
   );
 };
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,30 @@
+import Button from "../../components/Button";
+import landing_page_image from "../../resource/landing_page_image.svg";
+
 function Home() {
-  return <div>This is Home page.</div>;
+  return (
+    <div className=" mx-auto w-10/12">
+      <div className=" mt-20">
+        <h1 className=" font-bold text-5xl text-center block text-slate-800">
+          Imagine if <br />
+          <span className="font-bold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-violet-500 to-fuchsia-400">
+            Snapchat
+          </span>
+          <br />
+          had events.
+        </h1>
+      </div>
+      <p className="text-neutral-600 mt-5">
+        Easily host and share events with your friends across any social media.
+      </p>
+      <img
+        className="object-contain h-292 w-48 mx-auto mt-10"
+        src={landing_page_image}
+        alt="Event Card"
+      ></img>
+      <Button btnText="Create my event" />
+    </div>
+  );
 }
 
 export default Home;
